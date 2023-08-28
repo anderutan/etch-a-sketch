c31785d936aed79b3780a1e4ae4b57b20d18097b
anderutan <52438782+anderutan@users.noreply.github.com>
2023-08-28 14:42:01 +0800

    Add function on the button for color and reset

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+let color = 'black';
+
 document.addEventListener('DOMContentLoaded', function () {
   createBoard(16);
+
+  let btn_popup = document.querySelector('#popup');
+  btn_popup.addEventListener('click', function () {
+    let size = getSize();
+    createBoard(size);
+  });
 });
 
 function createBoard(size) {
@@ -10,7 +18,37 @@ function createBoard(size) {
   let numDivs = size * size;
   for (let i = 0; i < numDivs; i++) {
     let div = document.createElement('div');
-    div.style.backgroundColor = 'yellow';
+    div.addEventListener('mouseover', colorDiv);
     board.insertAdjacentElement('beforeend', div);
   }
+}
+
+function getSize() {
+  let userInput = prompt('What will be the size of the board?');
+  let message = document.querySelector('#message');
+  if (userInput === '') {
+    message.textContent = 'Please provide a number';
+  } else if (userInput < 2 || userInput > 100) {
+    message.textContent = 'Provide a number between 2 and 100';
+  } else {
+    message.textContent = 'Now you play!';
+    return userInput;
+  }
+}
+
+function colorDiv() {
+  if (color == 'random') {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = 'black';
+  }
+}
+
+function setColor(colorChoice) {
+  color = colorChoice;
+}
+
+function resetBoard() {
+  let divs = document.querySelectorAll('div');
+  divs.forEach((div) => (div.style.backgroundColor = 'white'));
 }
